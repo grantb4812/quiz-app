452f4deca86daa51507e3bba6ae8527139d11fd7
grantb4812 <grantb4812@gmail.com>
2015-08-26 17:27:56 -0500

    created question array of objects

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
-	
+	console.log(questions[4].answers[0]);	
+
 });
+
+var questions = [
+	{
+		number: "1",
+		question: "this might be a question?",
+		answers: ["a","b","c","d"],
+		correct: "a"
+	},
+		{
+		number: "2",
+		question: "this might be a question?",
+		answers: ["a","b","c","d"],
+		correct: "b"
+	},
+		{
+		number: "3",
+		question: "this might be a question?",
+		answers: ["a","b","c","d"],
+		correct: "c"
+	},
+		{
+		number: "4",
+		question: "this might be a question?",
+		answers: ["a","b","c","d"],
+		correct: "d"
+	},
+		{
+		number: "5",
+		question: "this might be a question?",
+		answers: ["a","b","c","d"],
+		correct: "a"
+	}
+];
