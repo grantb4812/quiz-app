7122611709c9b87886769626ce791a5506bbc7c8
grantb4812 <grantb4812@gmail.com>
2015-08-26 19:42:44 -0500

    added start quiz functionality and correct count

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-	$('.number').append(questions[0].number);
-	$('.quiz-question').append(questions[0].question);
+	$('.start-quiz-button').on('click', function(){
+			startQuiz();
+	})	
+
+	$('.submit-button').on('click', function() {
+		correctCount('a');
+	})
 
 });
 
@@ -36,3 +41,47 @@ var questions = [
 		correct: "a"
 	}
 ];
+
+var questionNumber;
+
+var startQuiz = function () {
+	
+		$('.app-start').hide();
+		$('.app-body').show();
+		$('.number').append(questions[0].number);
+		$('.quiz-question').append(questions[0].question);
+
+		questionNumber = questions[0].number;
+		
+		console.log(questionNumber);
+		
+		for (prop in questions[0].answers){
+			$('.answer ul').append("<li><span>" +questions[0].answers[prop]+ "</span><input type='radio'></li>");
+	}
+};
+
+
+
+var correctCount = function (answer) {
+	var incorrect = 0;
+	var correct = 0;
+
+	if (answer === questions[0].correct) {
+		correct++;
+		console.log(correct);
+		$('.correct').append(correct);
+	} else {
+		incorrect++;
+		$('.incorrect').append(incorrect);
+	}
+
+	
+
+};
+
+
+var submit = function () {
+
+	
+	
+};
