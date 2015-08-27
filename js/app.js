3b1089b2f804d0bab5387e485cd807c80f137767
grantb4812 <grantb4812@gmail.com>
2015-08-26 21:41:01 -0500

    able to submit and go to next question

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
 
 	$('.submit-button').on('click', function() {
 		correctCount('a');
+		showQuestion(questionNumber());
+		
+		
 	})
 
 });
@@ -12,48 +15,48 @@ $(document).ready(function(){
 var questions = [
 	{
 		number: "1",
-		question: "this might be a question?",
+		question: "this might be a question 1?",
 		answers: ["a","b","c","d"],
 		correct: "a"
 	},
 		{
 		number: "2",
-		question: "this might be a question?",
+		question: "this might be a question 2?",
 		answers: ["a","b","c","d"],
 		correct: "b"
 	},
 		{
 		number: "3",
-		question: "this might be a question?",
+		question: "this might be a question 3?",
 		answers: ["a","b","c","d"],
 		correct: "c"
 	},
 		{
 		number: "4",
-		question: "this might be a question?",
+		question: "this might be a question 4?",
 		answers: ["a","b","c","d"],
 		correct: "d"
 	},
 		{
 		number: "5",
-		question: "this might be a question?",
+		question: "this might be a question 5?",
 		answers: ["a","b","c","d"],
 		correct: "a"
 	}
 ];
 
-var questionNumber;
+var question;
 
 var startQuiz = function () {
 	
 		$('.app-start').hide();
 		$('.app-body').show();
 		$('.number').append(questions[0].number);
-		$('.quiz-question').append(questions[0].question);
-
-		questionNumber = questions[0].number;
+		$('.quiz-question p').append(questions[0].question);
+		// setting question to be used as index
+		question = questions[0].number - 1;
 		
-		console.log(questionNumber);
+		console.log(question);
 		
 		for (prop in questions[0].answers){
 			$('.answer ul').append("<li><span>" +questions[0].answers[prop]+ "</span><input type='radio'></li>");
@@ -68,8 +71,8 @@ var correctCount = function (answer) {
 
 	if (answer === questions[0].correct) {
 		correct++;
-		console.log(correct);
-		$('.correct').append(correct);
+		// console.log(correct);
+		$('.correct').replaceWith(correct);
 	} else {
 		incorrect++;
 		$('.incorrect').append(incorrect);
@@ -79,9 +82,34 @@ var correctCount = function (answer) {
 
 };
 
+var questionNumber = function() {
+	question++;
+	console.log(question);
+	return question;
+};
 
-var submit = function () {
-
+var showQuestion = function (n) {
+	// need to work on this
+			if (n > questions.length - 1) {
+				alert("Your done!")
+			}
 	
+			$('.number').empty();
+			$('.quiz-question p').empty();
+
+			$('.number').append(questions[n].number);
+			$('.quiz-question p').append(questions[n].question);
+			
+			$('.answer ul').empty();
+			for (prop in questions[prop].answers){
+				$('.answer ul').append("<li><span>" +questions[n].answers[prop]+ "</span><input type='radio'></li>");
+				console.log(questions[n].answers[prop]);
+			};
+	
+			
 	
 };
+
+// function that changes question number and returns the value
+// the showQuestion function takes that as an argument and is used for the index
+// stop when reach the end check lenght of array 
